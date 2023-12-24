4b8f8095ae6b7af27b13c0e2b6d0c18df4ad8d68
Hoang-Minh <quataoxanh83@gmail.com>
2023-12-24 11:23:28 -0800

    Sending POST request with order data

--- src/components/UI/Checkout.jsx
+++ src/components/UI/Checkout.jsx
@@ -24,13 +24,26 @@ const Checkout = () => {
     const fd = new FormData(event.target);
     const customerData = Object.fromEntries(fd.entries()); // {email: abc@example.com}
 
-    
-
-  }
+    fetch("http://localhost:3001/orders", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        order: {
+          items: cartCtx.items,
+          customer: customerData,
+        },
+      }),
+    });
+  };
 
   return (
-    <Modal open={userProgressCtx.progress === "checkout"} onClose={handleCloseCart}>
-      <form>
+    <Modal
+      open={userProgressCtx.progress === "checkout"}
+      onClose={handleCloseCart}
+    >
+      <form onSubmit={handleSubmit}>
         <h2>Checkout</h2>
         <p>Total Amount: {currencyFormatter.format(cartTotal)}</p>
         <Input label="Full Name" type="text" id="full-name" />
